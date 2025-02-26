a3a823c2a3143fb9fd8163d258da0ddcdc661eba
Sabrina <033.s.robinson@gmail.com>
2025-02-26 15:02:12 -0500

    converting basic project to a react-based project

---
+++ PreAlpha/app/src/App.js
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div>
+        <p>Hello World!</p>
+        <img src="https://i.pinimg.com/originals/88/14/9b/88149b0400750578f4d07d9bc3fb0fee.gif" />
+    
+        <p>BEauTifUl</p>
+        <h2>slay</h2>
+    </div>
+  );
+}
+
+export default App;
